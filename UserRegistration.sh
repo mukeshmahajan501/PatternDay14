941d52595286db4649321ba2ae17c75764ede96e
mukeshmahajan501 <mukeshmahajan501@gmail.com>
2020-07-12 11:04:51 +0530

    Update UserRegistration.sh

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -70,3 +70,18 @@ else
 fi
 
 
+#multiple email tested
+
+#!/bin/bash -x
+
+echo "enter email"
+read email
+
+
+pattern="^[0-9a-zA-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,}){1}*$"
+if [[ $email =~ $pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
