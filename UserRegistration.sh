7be0138c5ade5dc461d5b7e4e395afcb2fcd695d
mukesh <mukeshmahajan501@gmail.com>
2020-07-12 10:45:39 +0530

    validate password

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -49,3 +49,17 @@ fi
 
 
 
+#validate password with all cases
+
+#!/bin/bash -x
+echo "enter your password"
+read s
+
+if [[ ${#s} -ge 8 && "$s" == *[[:lower:]]* && "$s" == *[[:upper:]]* && "$s" == *[0-9]* && "$s" == *[@#%^-*+/]* ]]
+then
+        echo "valid password"
+else
+        echo "inavlid password"
+fi
+
+
