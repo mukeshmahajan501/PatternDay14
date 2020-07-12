d6a2f54954953b207dd0b6c883f4ed900b101c20
mukeshmahajan501 <mukeshmahajan501@gmail.com>
2020-07-12 10:30:45 +0530

    Update UserRegistration.sh

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,39 @@
+//validate firstname
+
+#!/bin/bash -x
+
+echo "enter first name :"
+read name
+
+pattern="^[[:upper:]]{1}[a-zA-Z]{3,}$"
+if [[ $name =~ $pattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
+
+
+
+
+//validate last name
+
+#!/bin/bash -x
+
+echo "enter words starting with capital "
+read word
+pat="^[[:upper:]]{1}[a-z]{3}"
+if [[ $word =~ $pat ]];
+then
+        echo "valid lastname";
+else
+        echo "invalid lastname";
+fi
+
+
+
+
+//validate email 
 #!/bin/bash -x
 
 echo "enter a email"
