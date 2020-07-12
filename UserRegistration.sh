04b129b253bd8486f63bb3be4df35c4aa7c290d1
mukesh <mukeshmahajan501@gmail.com>
2020-07-12 10:20:26 +0530

    validate last name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
 
-echo "enter first name :"
+echo "enter last name :"
 read name
 
 pattern="^[[:upper:]]{1}[a-zA-Z]{3,}$"
 if [[ $name =~ $pattern ]]
 then
-        echo "valid"
+        echo "valid lastname"
 else
-        echo "invalid"
+        echo "invalid lastname"
 fi
+
